9df0e65c3b569d94cd8c1b2af922fd5854ca8608
hashir-stack <hashir7ansari@gmail.com>
2024-11-26 01:54:44 +0530

    Adding unauthorized component.

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,56 @@
-import React from 'react';
-import './App.css';
-import Navbar from './Components/Navbar';
-import Table from './Components/Table';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import Layout from './Layout/Layout';
-import Home from './Components/Pages/Home';
-import ContactForm from './Components/ContactForm';
-import Profile from './Components/Profile';
-import Login from './Components/Pages/Login';
-
+import React from "react";
+import "./App.css";
+import Navbar from "./Components/Navbar";
+import Table from "./Components/Table";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Layout from "./Layout/Layout";
+import Home from "./Components/Pages/Home";
+import ContactForm from "./Components/ContactForm";
+import Profile from "./Components/Profile";
+import Login from "./Components/Pages/Login";
+import Unauth from "./Components/Unauth";
 
 const App = () => {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      errorElement: <h1>404 Error</h1>,
+      children: [
+        {
+          path: "/",
+          element: <Home />,
+        },
+        {
+          path: "/profile",
+          element: <Profile />,
+        },
+        {
+          path: "/contacts",
+          element: <Table />,
+        },
+        {
+          path: "/login",
+          element: <Login />,
+        },
 
-  const router = createBrowserRouter([{
-    path:"/",
-    element:<Layout/>,
-    errorElement:<h1>404 Error</h1>,
-    children:[
-      {
-        path: "/",
-        element: <Home/>,
-      },
-      {
-        path: "/profile",
-        element: <Profile/>,
-      },
-      {
-        path: "/contacts",
-        element:  <Table/>,
-      },
-      {
-        path: "/login",
-        element:  <Login/>,
-      },
-      
-      {
-        path: "/contactform",
-        element:  <ContactForm/>,
-      },
-     
-    ]
-  }])
+        {
+          path: "/contactform",
+          element: <ContactForm />,
+        },
+        {
+          path: "/unauth",
+          element: <Unauth />,
+        },
+      ],
+    },
+  ]);
 
   return (
     <>
       <RouterProvider router={router} />
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ src/Components/Unauth.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Unauth = () => {
+  return (
+    <div>Unauthorized</div>
+  )
+}
+
+export default Unauth;
